f715254abcff9828a88ef6c650b4ba00dc7729b6
akane-senseki <ajajggpw@gmail.com>
2021-02-10 20:37:18 +0900

    delete jsessionid

--- src/main/java/myapp/CommonConfig.java
+++ src/main/java/myapp/CommonConfig.java
@@ -1,6 +1,4 @@
 package myapp;
-
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,9 +6,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class CommonConfig extends WebSecurityConfigurerAdapter{
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
-      //  http.authorizeRequests().and().sessionManagement();
-        }
+    protected void configure(HttpSecurity http) throws Exception {
+        //SpringSecurityのデフォログイン画面の無効化
+      http.authorizeRequests().antMatchers("/*").permitAll().and().formLogin().and().httpBasic();
     }
+}
